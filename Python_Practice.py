30f05ab2680abe815ec8ec584eb53936ecbd3204
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-12-22 15:09:12 -0800

    Hi SKy

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,1 +1,18 @@
-print('Hello World')
+class Dog:
+
+    def __init__(self, age, name):
+        self._age = age
+        self._name = name
+
+    def getAge(self):
+        return self._age
+
+    def getName(self):
+        return self._name
+
+    def setAge(self, age):
+        self._age = age
+
+    def setName(self, name):
+        self._name = name
+           
