4710bd0e1da4dc8599583bb63d24c1adbd258868
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-12-22 15:25:50 -0800

    New

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,21 +1,3 @@
-class Dog:
+import tkinter as tk
 
-    def __init__(self, age, name):
-        self._age = age
-        self._name = name
-
-    def getAge(self):
-        return self._age
-
-    def getName(self):
-        return self._name
-
-    def setAge(self, age):
-        self._age = age
-
-    def setName(self, name):
-        self._name = name
-
-    def __str__(self):
-        return "dog:\nname: " + self._name + "/nage: " + str(self._age)
-    #what up
+class Application(tk.Frame):
