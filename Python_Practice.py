a481e4dcae437476a6b89f43fea886c1b1df380d
Sky Sylvanus <skysylvanus@Skys-MacBook-Air-5.local>
2018-12-22 16:46:12 -0800

    ugh

--- Python_Practice.py
+++ Python_Practice.py
@@ -14,16 +14,8 @@ class Application(tk.Frame):
         buttons = []
 
         for x in range(7):
-<<<<<<< HEAD
             buttons.append(tk.Button(self, text="Put Piece Here"))
             buttons[x].pack(side="left")
-=======
-            buttons.append(tk.Button(self))
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
->>>>>>> 056a56d1640e68494a53935b4ba1177e80ee8ed2
 
     def player_1(self):
         print("Player 1 went")
