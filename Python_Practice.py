43df8b7d28dd03b69788d1a0d68c38b0bedb16c9
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-12-22 16:13:30 -0800

    hi

--- Python_Practice.py
+++ Python_Practice.py
@@ -14,7 +14,7 @@ class Application(tk.Frame):
         buttons = []
 
         for x in range(7):
-            buttons.append(tk.Button(self))
+            buttons.append(tk.Button(self, text="Put Piece Here"))
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
