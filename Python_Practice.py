2f460fe241251a672f6cd3acf43e81cec631a79b
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-12-24 19:03:16 -0800

    hi

--- Python_Practice.py
+++ Python_Practice.py
@@ -24,56 +24,40 @@ class Application(tk.Frame):
 
                         """)
                 grid_layout.grid(row=i+2, column=j)
+
     def create_buttons(self):
         buttons = []
 
-
+        #Keeps track of whos turn it is and does stuff
+        def turn(column):
+            global turn_number
+            turn_number += 1
+            if turn_number % 2 == 0:
+                print("Player 1 went", column)
+            else:
+                print("Player 2 went", column)
+            self.isGameWon()
 
         #Creates a row of 7 buttons that do the turn method when clicked
         for x in range(7):
-            buttons.append(tk.Button(self, text="Put Piece Here",
-<<<<<<< HEAD
-                                     command=self.turn))
-            buttons[x].pack(side="left")
-=======
-                                     command=turn))
+            buttons.append(tk.Button(self, text=x, command= lambda: turn(x)))
             buttons[x].grid(row=9,column=x)
->>>>>>> ba48e5fe22145a9cf01195010424a2e3be02100c
 
-    #Keeps track of whos turn it is and does stuff
-    def turn():
-        global turn_number
-        turn_number += 1
-        if turn_number % 2 == 0:
-            print("Player 1 went")
-        else:
-            print("Player 2 went")
-        self.isGameWon()
-
-    def isGameWon():
-        return false
+    def isGameWon(self):
+        print()
 
 #Global variables
 
 #Keeps track of where pieces are, 0 means none, 1 means player 1
 #and 2 means player 2
-<<<<<<< HEAD
 board_layout = [[0 for x in range(7)] for y in range(6)]
 print(board_layout)
-=======
-    #board_layout = [][]
-    ###for x in range(7):
-        ###buttons.append()
-###
->>>>>>> ba48e5fe22145a9cf01195010424a2e3be02100c
 
-            turn_number = 0
+turn_number = 0
 
 root = tk.Tk()
 root.title("Connect 4")
 root.geometry('800x600')
-
-
 
 app = Application(master=root)
 app.mainloop()
