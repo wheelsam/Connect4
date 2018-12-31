f3184114e87ebcb9edf3596ecd8e35a58c0c4ab6
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-12-30 21:26:33 -0800

    Big moves

--- Python_Practice.py
+++ Python_Practice.py
@@ -10,64 +10,69 @@ class Application(Frame):
         self.master = master
         self.pack()
         self.create_board()
+        self.create_buttons()
 
     def create_board(self):
-        self.create_buttons()
         grid_layout = []
 
-
-
-        Lbl = Label(self, bg = "Green", text = "C4 demo")
+        Lbl = Label(self, bg = "White", text = "\n Connect Four \n")
         Lbl.grid(row = 1, column = 3)
 
-        for i in range(1,7):
+        for i in range(6):
             for j in range(7):
-                grid_layout = Label(self, text = """
-
-                            """, bg="Gray",relief="groove")
+                if board_layout[i][j] == 0:
+                    grid_layout = Label(self, text = "\t\t\n\n\n", bg="Gray",relief="groove")
+                if board_layout[i][j] == 1:
+                    grid_layout = Label(self, text = "\t\t\n\n\n", bg="Blue",relief="groove")
+                if board_layout[i][j] == 2:
+                    grid_layout = Label(self, text = "\t\t\n\n\n", bg="Red",relief="groove")
                 grid_layout.grid(row = i + 2, column = j)
 
-    def create_buttons(self):
-        buttons = []
+        #Making the changing player turn
         player_text = StringVar()
         player_turn = Label(self, textvariable = player_text)
-        player_text.set("Player 1\n Go")
-        player_turn.grid(row = 15, column = 3)
-
-        for x in range(7):
-            buttons.append(Button(self, text = "Go Here", command = lambda
-                                  column = x: self.turn(column, player_text)))
-            buttons[x].grid(row = 9, column = x)
-
-    #Keeps track of whos turn it is. Puts a number in a column depending on which button is pressed
-    def turn(self, column, player_text):
-        global board_layout
-        global turn_number
-        turn_number += 1
-        cs = 0
-        r = 0
-
-        while cs == 0:
-            if board_layout[r][column] == 0:
-                board_layout [r][column] = turn_number % 2 + 1
-                for y in range(6):
-                    print(str(board_layout[y]) + "\n")
-                cs = 1
-            else:
-                if r == 5:
-                    turn_number += 1
-                    return
-                r += 1
-
         if turn_number % 2 == 1:
             player_text.set("Player 1\n Go")
         else:
             player_text.set("Player 2\n Go")
+        player_turn.grid(row = 15, column = 3)
 
-        def isGameWon():
-            print(1)
 
-        isGameWon()
+    def create_buttons(self):
+        buttons = []
+
+        for x in range(7):
+            buttons.append(Button(self, text = "Go Here", command = lambda
+                                  column = x: self.turn(column)))
+            buttons[x].grid(row = 9, column = x)
+
+    #Keeps track of whos turn it is. Puts a number in a column depending on which button is pressed
+    def turn(self, column):
+        global board_layout
+        global turn_number
+        turn_number += 1
+        cs = 0
+        r = 5
+
+        while cs == 0:
+            if board_layout[r][column] == 0:
+                board_layout [r][column] = turn_number % 2 + 1
+                #for y in range(6):
+                #    print(str(board_layout[y]) + "\n")
+                cs = 1
+            else:
+                if r == 0:
+                    turn_number += 1
+                    return
+                r -= 1
+
+        #Bad because it creates an array of 42 buttons each time a button is pressed
+        self.create_board()
+
+        self.isGameWon()
+
+    def isGameWon(self):
+        x = 1
 
 
 root = Tk()
---
+++ twitterbot.py
@@ -0,0 +1,4 @@
+import webbrowser
+url = "http://www.google.com/"
+for x in range(1):
+    webbrowser.open_new_tab(url)
